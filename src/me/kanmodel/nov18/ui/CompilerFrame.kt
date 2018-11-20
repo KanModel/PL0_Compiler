4dffd55adf5765b9985001a5becf43430c3968d9
Kan_Model <kgdwhsk@gmail.com>
2018-11-20 14:59:00 +0800

    客户端添加编辑器

--- src/me/kanmodel/nov18/ui/CompilerFrame.kt
+++ src/me/kanmodel/nov18/ui/CompilerFrame.kt
@@ -3,9 +3,7 @@ package me.kanmodel.nov18.ui
 import compiler.error.Err
 import compiler.error.ErrorReason
 import compiler.PL0
-import java.awt.BorderLayout
-import java.awt.Component
-import java.awt.Dimension
+import java.awt.*
 import java.io.*
 import javax.swing.*
 
@@ -21,8 +19,18 @@ import javax.swing.*
  * @create: 2018-11-20 13:39
  */
 class CompilerFrame : JFrame() {
+    private val jTextArea: JTextArea = JTextArea(10, 40)
+    private var jScrollPane: JScrollPane = JScrollPane(jTextArea)
+    private var open: FileDialog = FileDialog(this, "打开文档", FileDialog.LOAD)
+    private var save: FileDialog = FileDialog(this, "保存文档", FileDialog.SAVE)
 
     init {
+        jTextArea.font = Font("Monospaced", 1, 20)
+        jTextArea.lineWrap = true//到达指定宽度则换行
+        jScrollPane.horizontalScrollBarPolicy = JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED
+        jScrollPane.verticalScrollBarPolicy = JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED
+        jScrollPane.setViewportView(jTextArea)
+
         val menuBar = JMenuBar()//菜单栏
         val fileMenu = JMenu("文件")
         val projectMenu = JMenu("项目")
@@ -42,9 +50,14 @@ class CompilerFrame : JFrame() {
         projectMenu.add(compileItem)
         projectMenu.add(runItem)
         helpMenu.add(aboutItem)
-
         openItem.addActionListener {
             showFileOpenDialog(this)
+        }
+        saveItem.addActionListener {
+            if (file == null) {
+                newFile()
+            }
+            saveFile()
         }
         compileItem.addActionListener {
             compileFile(fileString)
@@ -52,17 +65,136 @@ class CompilerFrame : JFrame() {
         runItem.addActionListener {
             run()
         }
+        closeItem.addActionListener { System.exit(0) }
+
 
         add(menuBar, BorderLayout.NORTH)
+        add(jScrollPane, BorderLayout.CENTER)
 
         title = "PL0 Compiler"
         defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
         size = Dimension(540, 270)
         setLocationRelativeTo(null)
-        isResizable = false
+//        isResizable = false
+    }
+
+    private fun compileFile(file: String) {
+        ErrorReason.init()
+
+        PL0.stdin = BufferedReader(InputStreamReader(System.`in`))
+        val fin: BufferedReader
+        try {
+            fin = BufferedReader(FileReader(file), 4096)
+
+            // 是否输出虚拟机代码
+            PL0.listSwitch = true
+
+            // 是否输出名字表
+            PL0.tableSwitch = true
+
+            PL0.sourcePrintStream = PrintStream("source.tmp")
+            PL0.sourcePrintStream.println("Input pl/0 file?   $file")
+
+            // 构造编译器并初始化
+            val pl0 = PL0(fin)
+
+            isCompileSuccess = pl0.compile()
+            if (isCompileSuccess) {
+                print("Compile succeed\n")
+            } else {
+                print("Errors in pl/0 program\n")
+                Err.showResult()
+            }
+
+        } catch (e: IOException) {
+            println("Can't open file!")
+        }
+    }
+
+    private fun run() {
+        if (isCompileSuccess) {
+            PL0.resultPrintStream = PrintStream("result.tmp")
+            PL0.interpreter.interpret()
+            PL0.resultPrintStream.close()
+        } else {
+            JOptionPane.showMessageDialog(frame, "请先编译成功后运行!", "警告", JOptionPane.WARNING_MESSAGE)
+        }
+    }
+
+    private fun showFileOpenDialog(parent: Component) {
+        /*val fileChooser = JFileChooser()
+
+        fileChooser.currentDirectory = File(".")
+
+        fileChooser.fileSelectionMode = JFileChooser.FILES_AND_DIRECTORIES
+        fileChooser.isMultiSelectionEnabled = false
+
+        val result = fileChooser.showOpenDialog(parent)
+
+        if (result == JFileChooser.APPROVE_OPTION) {
+            isCompileSuccess = false
+            file = fileChooser.selectedFile
+            val selectedFile = fileChooser.selectedFile
+            fileString = selectedFile.absolutePath
+            println(selectedFile.absolutePath)
+            println(selectedFile.parentFile)
+        }*/
+        open.isVisible = true
+
+        val dirPath = open.directory
+        val fileName = open.file
+        fileString = dirPath + fileName
+        println(dirPath)
+        println(fileName)
+        if (dirPath == null || fileName == null) {
+            return
+        }
+        file = File(dirPath, fileName)
+
+        jTextArea.text = ""//打开文件之前清空文本区域
+
+        try {
+            val br = BufferedReader(FileReader(file))
+            var line: String?
+            line = br.readLine()
+            while (line != null) {
+                //将给定文本追加到文档结尾。如果模型为 null 或者字符串为 null 或空，则不执行任何操作。
+                //虽然大多数 Swing 方法不是线程安全的，但此方法是线程安全的。
+                jTextArea.append(line + "\r\n")
+                line = br.readLine()
+            }
+        } catch (ex: IOException) {
+            throw RuntimeException("读取失败！")
+        }
+    }
+
+    //新建一个文件
+    private fun newFile() {
+        if (file == null) {
+            save.isVisible = true
+            val dirPath = save.directory
+            val fileName = save.file
+            if (dirPath == null || fileName == null) {
+                return
+            }
+            file = File(dirPath, fileName)
+        }
+    }
+
+    //保存文件
+    private fun saveFile() {
+        try {
+            val bw = BufferedWriter(FileWriter(file))
+            val text = jTextArea.text
+            bw.write(text)
+            bw.close()
+        } catch (ex: IOException) {
+            throw RuntimeException()
+        }
     }
 
     companion object {
+        private var file: File? = null
         var isCompileSuccess = false
         lateinit var fileString: String
         val frame = CompilerFrame()
@@ -71,67 +203,6 @@ class CompilerFrame : JFrame() {
         @JvmStatic
         fun main(args: Array<String>) {
             frame.isVisible = true
-        }
-
-        fun compileFile(file: String) {
-            ErrorReason.init()
-
-            val fin: BufferedReader
-            try {
-                fin = BufferedReader(FileReader(file), 4096)
-
-                // 是否输出虚拟机代码
-                PL0.listSwitch = true
-
-                // 是否输出名字表
-                PL0.tableSwitch = true
-
-                PL0.sourcePrintStream = PrintStream("source.tmp")
-                PL0.sourcePrintStream.println("Input pl/0 file?   $file")
-
-                // 构造编译器并初始化
-                val pl0 = PL0(fin)
-
-                isCompileSuccess = pl0.compile()
-                if (isCompileSuccess) {
-                    print("Compile succeed\n")
-                } else {
-                    print("Errors in pl/0 program\n")
-                    Err.showResult()
-                }
-
-            } catch (e: IOException) {
-                println("Can't open file!")
-            }
-        }
-
-        fun run() {
-            if (isCompileSuccess) {
-                PL0.resultPrintStream = PrintStream("result.tmp")
-                PL0.interpreter.interpret()
-                PL0.resultPrintStream.close()
-            } else {
-                JOptionPane.showMessageDialog(frame, "请先编译成功后运行!", "警告", JOptionPane.WARNING_MESSAGE)
-            }
-        }
-
-        fun showFileOpenDialog(parent: Component) {
-            val fileChooser = JFileChooser()
-
-            fileChooser.currentDirectory = File(".")
-
-            fileChooser.fileSelectionMode = JFileChooser.FILES_AND_DIRECTORIES
-            fileChooser.isMultiSelectionEnabled = false
-
-            val result = fileChooser.showOpenDialog(parent)
-
-            if (result == JFileChooser.APPROVE_OPTION) {
-                isCompileSuccess = false
-                val file = fileChooser.selectedFile
-                fileString = file.absolutePath
-                println(file.absolutePath)
-                println(file.parentFile)
-            }
         }
     }
 }
