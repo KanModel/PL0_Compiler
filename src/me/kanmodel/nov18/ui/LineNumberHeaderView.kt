8711f9e1ddfe3a78dbcac36894ffaaed14f193db
KanModel <136995219@qq.com>
2019-01-08 16:25:34 +0800

    调整行号显示样式

--- src/me/kanmodel/nov18/ui/LineNumberHeaderView.kt
+++ src/me/kanmodel/nov18/ui/LineNumberHeaderView.kt
@@ -1,27 +1,15 @@
 package me.kanmodel.nov18.ui
 
-/**
- * Created with IntelliJ IDEA.
- * Description:
- * User: KanModel
- * Date: 2019-01-08-15:36
- */
-
 import java.awt.Color
 import java.awt.Dimension
 import java.awt.Font
 import java.awt.FontMetrics
 import java.awt.Graphics
-import java.awt.Rectangle
 
-//TEXTAREA 行号显示插件
 class LineNumberHeaderView : javax.swing.JComponent() {
-    //    private final  Font DEFAULT_FONT = new Font(Font.MONOSPACED, Font.PLAIN, 11);
-    private val DEFAULT_FONT = Font("Monospaced", 1, 20)
-    val DEFAULT_BACKGROUD = Color(228, 228, 228)
-    val DEFAULT_FOREGROUD = Color.BLACK
-    val nHEIGHT = Integer.MAX_VALUE - 1000000
-    val MARGIN = 5
+    private val defaultFont = Font("Monospaced", 1, 20)
+    private val defaultBackground = Color(228, 228, 228)
+    private val defaultForeground = Color.BLACK!!
     private var lineHeight: Int = 0
     private var fontLineHeight: Int = 0
     private var currentRowWidth: Int = 0
@@ -31,15 +19,14 @@ class LineNumberHeaderView : javax.swing.JComponent() {
         get() = 4
 
     init {
-        font = DEFAULT_FONT
-        foreground = DEFAULT_FOREGROUD
-        background = DEFAULT_BACKGROUD
-        setPreferredSize(9999)
+        font = defaultFont
+        foreground = defaultForeground
+        background = defaultBackground
+        setPreferredSize(4)
     }
 
-    fun setPreferredSize(row: Int) {
+    private fun setPreferredSize(row: Int) {
         val width = fontMetrics!!.stringWidth(row.toString())
-        println("width$width")
         if (currentRowWidth < width) {
             currentRowWidth = width
             preferredSize = Dimension(2 * MARGIN + width + 1, nHEIGHT)
@@ -52,7 +39,7 @@ class LineNumberHeaderView : javax.swing.JComponent() {
         fontLineHeight = fontMetrics!!.height
     }
 
-    fun getLineHeight(): Int {
+    private fun getLineHeight(): Int {
         return if (lineHeight == 0) {
             fontLineHeight
         } else lineHeight
@@ -65,29 +52,27 @@ class LineNumberHeaderView : javax.swing.JComponent() {
     }
 
     override fun paintComponent(g: Graphics) {
-        val nlineHeight = getLineHeight()
+        val nLineHeight = getLineHeight()
         val startOffset = startOffset
         val drawHere = g.clipBounds
         g.color = background
         g.fillRect(drawHere.x, drawHere.y, drawHere.width, drawHere.height)
         g.color = foreground
-        val startLineNum = drawHere.y / nlineHeight + 1
-        val endLineNum = startLineNum + drawHere.height / nlineHeight
-        var start = drawHere.y / nlineHeight * nlineHeight + nlineHeight - startOffset
+        val startLineNum = drawHere.y / nLineHeight + 1
+        val endLineNum = startLineNum + drawHere.height / nLineHeight
+        var start = drawHere.y / nLineHeight * nLineHeight + nLineHeight - startOffset
         for (i in startLineNum..endLineNum) {
             val lineNum = i.toString()
             val width = fontMetrics!!.stringWidth(lineNum)
             g.drawString("$lineNum ", MARGIN + currentRowWidth - width - 1, start)
-            start += nlineHeight
+            start += nLineHeight
         }
         setPreferredSize(endLineNum)
     }
 
     companion object {
-
-        /**
-         * JAVA TextArea行数显示插件
-         */
-        private val serialVersionUID = 1L
+        private const val serialVersionUID = 1L
+        const val MARGIN = 5
+        const val nHEIGHT = Integer.MAX_VALUE - 1000000
     }
 }
